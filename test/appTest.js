5823bfd70f62ea91a76cc45b29ca1701ebf9cd03
Rama Ferguson <ferguson.rama@gmail.com>
2019-05-02 16:18:50 -0700

    added some tests for player class

--- test/appTest.js
+++ test/appTest.js
@@ -1,21 +1,45 @@
 const assert = require('chai').assert;
-//const app = require('./server');
-const sayHello = require('./server').sayHello;
-const addNumbers = require('./server').addNumbers;
+const Player = require('../views/JS/Clicker').Player;
 
-describe('App', () => {
-  it('sayHello should return hellos', () => {
-    let result = sayHello();
-    assert.equal(result, 'hellos');
-  });
+// Testing Player class
+describe('Player', () => {
+  
+  // Testing 
+  describe('#constructor', () => {
+    let player = new Player('bang', 30, 15, 10);
 
-  it('sayHello should return type string', () => {
-    let result = sayHello();
-    assert.typeOf(result, 'string')
-  });
+    // Test valid 
+    it('constructor should save username as a string', () => {
+      assert.typeOf(player.name, 'string');
+    });
 
-  it('addNumbers should be greater than 5', () => {
-    let result = addNumbers(-1, 5);
-    assert.isAbove(result, 5);
+    it('constructor should save username as \'bang\'', () => {
+      assert.equal(player.name, 'bang');
+    });
+
+    it('constructor should save hp as an int', () => {
+      assert.typeOf(player.name, 'bang');
+    });
   });
 });
+
+//const app = require('./server');
+// const sayHello = require('./server').sayHello;
+// const addNumbers = require('./server').addNumbers;
+
+// describe('App', () => {
+//   it('sayHello should return hellos', () => {
+//     let result = sayHello();
+//     assert.equal(result, 'hellos');
+//   });
+
+//   it('sayHello should return type string', () => {
+//     let result = sayHello();
+//     assert.typeOf(result, 'string')
+//   });
+
+//   it('addNumbers should be greater than 5', () => {
+//     let result = addNumbers(-1, 5);
+//     assert.isAbove(result, 5);
+//   });
+// });
