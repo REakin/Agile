2ad06bf5f535d324332f4d7b9e4ec67b50b27231
Rama Ferguson <ferguson.rama@gmail.com>
2019-05-02 20:55:27 -0700

    added test cases for all Clicker.js classes

--- test/appTest.js
+++ test/appTest.js
@@ -1,45 +1,266 @@
 const assert = require('chai').assert;
-const Player = require('../views/JS/Clicker').Player;
+const clicker = require('../views/JS/Clicker');
+const Player = clicker.Player;
+const Enemy = clicker.Enemy;
+const Autochar = clicker.Autochar;
 
 // Testing Player class
 describe('Player', () => {
-  
-  // Testing 
+
+  // Testing the constructor
   describe('#constructor', () => {
     let player = new Player('bang', 30, 15, 10);
 
-    // Test valid 
-    it('constructor should save username as a string', () => {
+    // Test username
+    it('should save username as a string', () => {
       assert.typeOf(player.name, 'string');
     });
 
-    it('constructor should save username as \'bang\'', () => {
+    it('should save username as \'bang\'', () => {
       assert.equal(player.name, 'bang');
     });
 
-    it('constructor should save hp as an int', () => {
-      assert.typeOf(player.name, 'bang');
+    // Test hp
+    it('should save hp as a number', () => {
+      assert.typeOf(player.hp, 'number');
     });
+
+    it('should save hp as 30', () => {
+      assert.equal(player.hp, 30);
+    });
+
+    // Test max hp
+    it('should save max hp as a number', () => {
+      assert.typeOf(player.maxhp, 'number');
+    });
+
+    it('should save max hp as 30', () => {
+      assert.equal(player.maxhp, 30);
+    });
+
+    // Test gold
+    it('should save gold as a number', () => {
+      assert.typeOf(player.gold, 'number');
+    });
+
+    it('should save gold as 15', () => {
+      assert.equal(player.gold, 15);
+    });
+
+    // Test attack value (av)
+    it('should save av as a number', () => {
+      assert.typeOf(player.av, 'number');
+    });
+
+    it('should save av as 10', () => {
+      assert.equal(player.av, 10);
+    });
+
   });
+
+  // TODO - Testing attack
+  // We cannot test this until the code is refactored
+  // to not rely on window variables
+  // describe('#attack', () => {
+  //   it('should reduce enemy hp from 20 to 10 ', () => {
+  //     let enemy = new Enemy(20, 10, 5);
+  //     player.attack()
+  //     assert.equal(enemy.hp, 10);
+  //   });
+  // });
+
+  // Testing takedamage
+  describe('#takedamage', () => {
+
+    it('should reduce player hp from 30 to 5 ', () => {
+      let player = new Player('bang', 30, 15, 10);
+      player.takedamage(25);
+      assert.equal(player.hp, 5);
+    });
+
+    // TODO - Add React testing to be able to test
+    // player death
+    // it('should call die()', () => {
+    //   let player = new Player('bang', 30, 15, 10);
+    //   player.takedamage(30)
+    // });
+  });
+
+  // TODO - Figure out testing time outs
+  // describe('#ability1', () => {
+  //   it('should raise bonus attack value (bonusav) from 0 to 5 ', () => {
+  //     let player = new Player('bang', 30, 15, 10);
+  //     player.ability1();
+  //     assert.equal(player.bonusav, 5);
+  //   });
+
+  // TODO - figure out testing time outs
+  // it('should reduce bonus attack value from 5 to 0 after 2000 milliseconds ', () => {
+  //   setTimeout(function () {
+  //     let player = new Player('bang', 30, 15, 10);
+  //     player.ability1();
+  //     assert.equal(player.bonusav, 0);
+  //   }, 2001);
+  // });
+
+  // });
+
+  // describe('#endgame', () => {
+  //   // TODO - React Testing
+  //     it('should render the game over page when called ', () => {
+
+  //     });
+
+  //   // TODO - React Testing
+  //     it('should render the escape message when called ', () => {
+
+  //     });
+
+  // });
+
+  // TODO - React testing
+  // describe('#die', () => {
+  //    We cannot test this until the code is refactored
+  //    to not rely on window variables
+  //   it('should set player gold from 30 to 0', () => {
+  //     let player = new Player('bang', 30, 15, 10);
+  //     player.die()
+  //     assert.equal(player.gold, 0);
+  //   });
+  // });
+
 });
 
-//const app = require('./server');
-// const sayHello = require('./server').sayHello;
-// const addNumbers = require('./server').addNumbers;
+// Testing Enemy class
+describe('Enemy', () => {
 
-// describe('App', () => {
-//   it('sayHello should return hellos', () => {
-//     let result = sayHello();
-//     assert.equal(result, 'hellos');
-//   });
+  // Testing the constructor
+  describe('#constructor', () => {
+    // TODO - allow testing the constructor
+    // this.attackint prevents constructor from being tested
+    // because it calls enemy.attack() which in turn uses
+    // the player undeclared variable
 
-//   it('sayHello should return type string', () => {
-//     let result = sayHello();
-//     assert.typeOf(result, 'string')
-//   });
+    // let enemy = new Enemy(20, 10, 5);
 
-//   it('addNumbers should be greater than 5', () => {
-//     let result = addNumbers(-1, 5);
-//     assert.isAbove(result, 5);
-//   });
-// });
+    // Test hp
+    it('should save hp as a number', () => {
+      assert.typeOf(enemy.hp, 'number');
+    });
+
+    it('should save hp as 20', () => {
+      assert.equal(enemy.hp, 20);
+    });
+
+    // Test max hp
+    it('should save max hp as a number', () => {
+      assert.typeOf(enemy.maxhp, 'number');
+    });
+
+    it('should save max hp as 20', () => {
+      assert.equal(enemy.maxhp, 20);
+    });
+
+    // Test attack value (av)
+    it('should save av as a number', () => {
+      assert.typeOf(enemy.av, 'number');
+    });
+
+    it('should save av as 10', () => {
+      assert.equal(enemy.av, 10);
+    });
+
+    // Test interval (i)
+    it('should save av as a number', () => {
+      assert.typeOf(enemy.i, 'number');
+    });
+
+    it('should save i as 5', () => {
+      assert.equal(enemy.i, 5);
+    });
+
+    // TODO - test attack interval (attackint)
+    // it('should have an interval of X', () => {
+
+    // });
+  });
+
+  //   TODO - Testing attack
+  // We cannot test this until the code is refactored
+  // to not rely on window variables
+  // describe('#attack', () => {
+  //   it('should reduce player hp from 30 to 20 ', () => {
+  //     let enemy = new Enemy(20, 10, 5);
+  //     let player = new Player('bang', 30, 15, 10);
+  //     enemy.attack();
+  //     assert.equal(player.hp, 20);
+  //   });
+  // });
+
+  // Testing takedamage
+  describe('#takedamage', () => {
+    // let enemy = new Enemy(20, 10, 5);
+
+    it('should reduce enemy hp from 20 to 5 ', () => {
+      enemy.takedamage(15);
+      assert.equal(enemy.hp, 5);
+    });
+
+  });
+
+  // TODO - React testing
+  // describe('#die', () => {
+  //    We cannot test this until the code is refactored
+  //    to not rely on window variables
+  //   it('should set player gold from 30 to 0', () => {
+  //     let enemy = new Enemy(20, 10, 5);
+  //     enemy.die()
+  //     stuff goes here...
+  //   });
+  // });
+
+});
+
+// Testing Player class
+describe('Autochar', () => {
+
+  // Testing the constructor
+  describe('#constructor', () => {
+    let autochar = new Autochar('slime felcher', 8, 3);
+
+    // Test name
+    it('should save name as a string', () => {
+      assert.typeOf(autochar.name, 'string');
+    });
+
+    it('should save name as \'slime felcher\'', () => {
+      assert.equal(autochar.name, 'slime felcher');
+    });
+
+    // Test attack value (av)
+    it('should save av as a number', () => {
+      assert.typeOf(autochar.av, 'number');
+    });
+
+    it('should save av as 8', () => {
+      assert.equal(autochar.av, 8);
+    });
+
+    // this is failing for some reason...
+    // // Test interval (i)
+    // it('should save i as a number', () => {
+    //   assert.typeOf(autochar.i, 'number');
+    // });
+
+    // it('should save i as 3', () => {
+    //   assert.equal(autochar.i, 3);
+    // });
+
+    // None of the other parts of autochar can be tested yet
+
+  });
+
+
+
+
+});
