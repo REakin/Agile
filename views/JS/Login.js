f0a47682b744dcd75a6f6514059876c81fc1ece9
Ryan Eakin <Eakinryan@hotmail.com>
2019-05-13 08:46:37 -0700

    fixed something

--- views/JS/Login.js
+++ views/JS/Login.js
@@ -1,32 +1,19 @@
 var login = new window.XMLHttpRequest();
 var register = new window.XMLHttpRequest();
 
-function handlesubmit() {
-    sendlogin((res) => {
-        console.log(res);
-        return res
-    });
-}
-
-function sendlogin(callback){
+function sendlogin(){
     let uname = document.getElementById('lusername').value;
     let password = document.getElementById('lpassword').value;
-    login.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            let result = JSON.parse(this.response);
-            if (result.auth === false) {
-                //alert('Login Failed');
-                callback(false)
-            }
-            if (result.auth === true) {
-                callback(true)
-            }
-        }
-    };
-    data = {username:uname,password:password};
-    login.open("post",`/logincheck`,true);
+    let data = {username:uname,password:password};
+    login.open("post",`/logincheck`,false);
     login.setRequestHeader('Content-Type','application/json');
     login.send(JSON.stringify(data));
+    let response = JSON.parse(login.response)
+    if (response.auth == true){
+        return true
+    }else{return false}
+
+
 }
 
 function sendRegister(){
